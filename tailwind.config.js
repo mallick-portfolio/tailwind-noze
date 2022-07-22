c84743a00eb25cf00156fc0b586947ca24882779
mallick-portfolioE <tamal1714019@gmail.com>
2022-07-22 20:04:55 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#654a9b',
+        secondaary: '#b9b0e4',
+      },
+    },
+  },
+  plugins: [],
+}
